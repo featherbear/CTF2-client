4b9041807b924e3bf38ab38b9de0eb6e7a3a11bb
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-14 02:33:22 +1000

    Remove form parent

--- src/routes/invite/_components/LoginSection.svelte
+++ src/routes/invite/_components/LoginSection.svelte
@@ -7,10 +7,5 @@
   import { Input, InputBlue } from "../../../ui/components/Input";
 </script>
 
-<form autocomplete="off">
-  <Input type="text" pattern="\w." maxlength="20" placeholder="username" />
-
-  <InputBlue type="password" placeholder="password" />
-
-  <span on:click={() => dispatch('changeState', STATE.METHOD)}>back</span>
-</form>
+<Input type="text" pattern="\w." maxlength="20" placeholder="username" />
+<InputBlue type="password" placeholder="password" />
