4a90ee23e1005b6d79c194225c0d1b3b0837f32b
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-14 02:31:35 +1000

    Update styling

--- src/ui/components/Input/Minimal.svelte
+++ src/ui/components/Input/Minimal.svelte
@@ -1,6 +1,13 @@
 <style>
   .minimal {
     background-color: transparent;
+    padding: 0;
+    outline: none;
+    box-shadow: none;
+    border: none;
+    border-bottom: 5px solid white;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
     outline: none;
     border: none;
     border-bottom: 5px solid white;
