65f243b0a4459da60bba5700ae3ca861cc333527
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-28 00:39:56 +1000

    Housekeeping

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,21 +1,15 @@
-
-
 <script>
-  // import Nav from "../components/Nav.svelte";
-
+  // import Nav from "../ui/components/Nav.svelte";
   export let segment;
-  console.log("layout PROPS", $$props)
-
 </script>
 
 <style lang="scss" global>
   @import "../ui/style/index.scss";
 </style>
 
-{#if [undefined, 'invite'].includes(segment) }
+{#if [undefined, 'invite', 'debug'].includes(segment) }
   <slot />
 {:else}
   <!-- <Nav {segment} /> -->
-  Layout
   <slot />
 {/if}
