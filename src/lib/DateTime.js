0f7a92f2cc49cfc3e479c7d93ba89b86f17b26d9
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-28 02:06:09 +1000

    Code formatting

--- src/lib/DateTime.js
+++ src/lib/DateTime.js
@@ -39,24 +39,27 @@ export const secondAccurate = readable(new Date(), set => {
 
 let minuteAccurateDate
 let minuteAccurateTimer
-export const minuteAccurate = readable((minuteAccurateDate = new Date()), (set) => {
-  (function setupTimer () {
-    minuteAccurateTimer = setTimeout(() => {
-      set((minuteAccurateDate = new Date()))
-      setupTimer()
-    }, 60000 - minuteAccurateDate.getTime() % 60000)
-  })()
-  return () => clearInterval(minuteAccurateTimer)
-})
+export const minuteAccurate = readable(
+  (minuteAccurateDate = new Date()),
+  set => {
+    ;(function setupTimer () {
+      minuteAccurateTimer = setTimeout(() => {
+        set((minuteAccurateDate = new Date()))
+        setupTimer()
+      }, 60000 - (minuteAccurateDate.getTime() % 60000))
+    })()
+    return () => clearInterval(minuteAccurateTimer)
+  }
+)
 
 let hourAccurateDate
 let hourAccurateTimer
-export const hourAccurate = readable((hourAccurateDate = new Date()), (set) => {
-  (function setupTimer () {
+export const hourAccurate = readable((hourAccurateDate = new Date()), set => {
+  ;(function setupTimer () {
     hourAccurateTimer = setTimeout(() => {
       set((hourAccurateDate = new Date()))
       setupTimer()
-    }, 3600000 - hourAccurateDate.getTime() % 3600000)
+    }, 3600000 - (hourAccurateDate.getTime() % 3600000))
   })()
   return () => clearInterval(hourAccurateTimer)
 })
