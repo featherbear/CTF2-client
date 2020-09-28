7f08258ab4d4adfe7318a1df7db1236272878e24
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-28 17:10:10 +1000

    Refactor server-side fetch routine for backend connectivity check

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import sirv from 'sirv'
 import express from 'express'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
-const { createProxyMiddleware } = require('http-proxy-middleware')
-import Fetcher from './lib/Fetcher'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import BackendLink from './lib/BackendLink'
 
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
@@ -12,45 +12,38 @@ require('dotenv').config()
 
 const { CTF2_ENDPOINT } = process.env
 
+express()
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    createProxyMiddleware('/proxy', {
+      target: CTF2_ENDPOINT,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/proxy': ''
+      },
+      logLevel: 'error'
+    }),
+    sapper.middleware({
+      session: (req, res) => {
+        return {
+          // TODO: Session
+        }
+      }
+    })
+  )
+  .listen(PORT, err => {
+    if (err) {
+      console.error('Error starting Project CTF² frontend server: ', err)
+    } else {
+      console.info('Project CTF² frontend server started')
+    }
+  })
+
 if (!CTF2_ENDPOINT) {
   console.info('CTF2_ENDPOINT not set up! Please configure')
   process.exit(0)
 } else {
-  console.info(`Connecting to CTF² server at ${CTF2_ENDPOINT}`)
-
-  const fetch = new Fetcher(CTF2_ENDPOINT)
-  fetch('hello')
-    .then(r => r.json())
-    .then(d => {
-      // Expose a fetch() wrapper for all CTF2 server endpoints
-      global.CTF2_fetch = fetch
-    })
-    .catch(e => {
-      console.error('Could not establish a connection to the server: ' + e.code)
-      process.exit(1)
-    })
-    .then(() =>
-      express()
-        .use(
-          compression({ threshold: 0 }),
-          sirv('static', { dev }),
-          createProxyMiddleware('/proxy', {
-            target: CTF2_ENDPOINT,
-            changeOrigin: true,
-            pathRewrite: {
-              '^/proxy': ''
-            },
-            logLevel: 'error'
-          }),
-          sapper.middleware({
-            session: (req, res) => ({
-              // TODO: Session
-            })
-          })
-        )
-        .listen(PORT, err => {
-          console.info('Server started')
-          if (err) console.error('Error starting web server', err)
-        })
-    )
+  console.info(`Connecting to Project CTF² backend server at ${CTF2_ENDPOINT}`)
+  BackendLink.begin(CTF2_ENDPOINT)
 }
